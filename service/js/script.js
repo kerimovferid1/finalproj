cdfa361466eb87ef70681a30c4b99ca0e5ab5f7c
Ferid <kerimovferid98@gmail.com>
2017-04-09 22:40:39 +0400

    proj

--- service/js/script.js
+++ service/js/script.js
@@ -85,3 +85,14 @@ $('.dot').click(function(){
 //     // console.log(d);
 //     })
 // }
+// function initMap() {
+//         var uluru = {lat: -25.363, lng: 131.044};
+//         var map = new google.maps.Map(document.getElementById('map'), {
+//           zoom: 4,
+//           center: uluru
+//         });
+//         var marker = new google.maps.Marker({
+//           position: uluru,
+//           map: map
+//         });
+//       }
